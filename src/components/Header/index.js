ad41c2f5fb879db088ddff2483eaef927a5f46a6
esterlilianlb <esterlilian2231@gmail.com>
2020-03-28 20:18:36 -0300

    new header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaInfoCircle, FaUserCircle, FaChevronDown } from 'react-icons/fa';
+
+import './styles.css';
+import logoImg from '../../images/logoquerobolsa.svg';
+
+export default function Header() {
+    return(
+        <header>
+          <div className="main">
+            <div className="option">
+              <FaInfoCircle size={30} color="#007a8d" />
+              <p>Ajuda</p>
+            </div>
+
+            <img className="header-logo" src={logoImg} alt="Quero Bolsa"/>
+
+            <div className="option">
+              <FaUserCircle size={30} color="#007a8d" />
+              <p>Conta</p>
+            </div>
+
+          </div>
+            <nav>
+              <ul>
+                <li>Minha conta</li>
+                <li>Menu <FaChevronDown size={10} color="#fff" /></li>
+              </ul>
+            </nav>
+      </header>
+    )
+}
