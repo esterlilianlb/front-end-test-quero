632493a76ab28799fcdb75cfd828b038b27a0744
esterlilianlb <esterlilian2231@gmail.com>
2020-03-28 22:00:34 -0300

    add favorites style ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import Header from './components/Header';
+import Main from './main';
 
 import './global.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      
-      
+      <Main />
     </div>
   );
 }
