cfaf79cb740a247e06550dc988479588f65341f7
esterlilianlb <esterlilian2231@gmail.com>
2020-03-29 16:07:11 -0300

    organizing folders

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from './components/Header';
-import Button from './components/Button';
-import Footer from './components/Footer';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Footer from '../components/Footer';
 
 import { FaChevronLeft, FaPlusCircle } from 'react-icons/fa';
 
