56bd85259e67e8ee2eed0ebb53836bf4dbbc26e2
esterlilianlb <esterlilian2231@gmail.com>
2020-03-30 12:08:39 -0300

    estilos aplicados no modal

--- src/pages/Modal.js
+++ src/pages/Modal.js
@@ -39,14 +39,23 @@ export default function MyModal() {
   // cards
   const card = db.map(item => {
     return(
-      <div className="university">
-            <input type="checkbox" name="" id=""/>
-            <img src={item.university.logo_url} alt="logo" width={70}/>
+      <div className="university-card">
+            <div className="select-option">
+              <input type="checkbox" name="" id="select-course"/>
+              <img src={item.university.logo_url} alt="logo" width={70}/>
+            </div>
             <span>
-              <p>{item.course.name}</p>
-              <p>{item.course.kind}</p>
+              <p className="course-name">{item.course.name}</p>
+              <p>{item.course.level}</p>
 
-    <p>Bolsa de {item.discount_percentage}%{item.price_with_discount}/mês</p>
+            <p className="percentage">Bolsa de 
+              <span className="green-text">
+                {item.discount_percentage}%
+              </span>
+              </p>
+              <p className="green-text" id="month-price">
+                {item.price_with_discount}/mês
+              </p>
             </span>
           </div>
     )
@@ -57,10 +66,10 @@ export default function MyModal() {
     <div>
       <FaPlusCircle size={80} color="#18acc4" onClick={() => setModalIsOpen(true)} />
       
-    <Modal isOpen={modalIsOpen} ariaHideApp={false}>
-    <span className="close" onClick={() => setModalIsOpen(false)}>&times;</span>
-      <h1>Adicionar bolsa</h1>
-      <p>Filtre e adicione as bolsas de seu interesse.</p>
+      <Modal isOpen={modalIsOpen} ariaHideApp={false}>
+      <span className="close" onClick={() => setModalIsOpen(false)}>&times;</span>
+        <h1>Adicionar bolsa</h1>
+        <p>Filtre e adicione as bolsas de seu interesse.</p>
 
       <form>
         <label htmlFor="city-select">SELECIONE SUA CIDADE</label>
@@ -69,11 +78,13 @@ export default function MyModal() {
         <label htmlFor="course-input">SELECIONE O CURSO DE SUA PREFERÊNCIA</label>
         <select className="select" id="course-select">{courses}</select>
 
-        <label htmlFor="course-type">COMO VOCÊ QUER ESTUDAR?</label>
-        <input type="checkbox" name="Presencial" title="Presencial" id="course-type presencial"/>
-        <label htmlFor="presencial">Presencial</label>
-        <input type="checkbox" name="A distância" title="A distância" id="course-type distancia"/>
-        <label htmlFor="distancia">A distância</label>
+        <label htmlFor="modality">COMO VOCÊ QUER ESTUDAR?</label>
+        <div className="modality">
+          <input type="checkbox" name="Presencial" title="Presencial" id="course-type presencial"/>
+          <label htmlFor="presencial">Presencial</label>
+          <input type="checkbox" name="A distância" title="A distância" id="course-type distancia"/>
+          <label htmlFor="distancia">A distância</label>
+        </div>
 
         <label htmlFor="price">ATÉ QUANTO PODE PAGAR?</label>
         <input type="range" name="Price" id="price" min="0" max="10000" step="100"/>
